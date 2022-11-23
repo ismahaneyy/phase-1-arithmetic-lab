cbd8527b9cc174e0a4da7dcbbaa34db5efbce756
ismahaneyy <liciousmayrah@gmail.com>
2022-11-23 21:48:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+//function(multiply)
+let num1 = 1 ;
+let num2 = 62;
+const multiply = num1 * num2;
 
-// Write your code here
+//function(random)
+let number = 5;
+const random = 1
+
+//function(mod)
+let num3 = 29;
+let num4 = 5;
+const mod = num3%num4
+
+//function(max)
+const array = [14,7,20,9];
+const max = Math.max(...array)
+
+
+
+
+
